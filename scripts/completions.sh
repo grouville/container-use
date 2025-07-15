67f5e6e53586c7920a256bc3e62da6dfe12c99ee
Guillaume de Rouville <guillaume@dagger.io>
2025-07-15 08:28:47 -0700

    chore: remove even more
    Signed-off-by: Guillaume de Rouville <guillaume@dagger.io>

--- scripts/completions.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-rm -rf completions
-mkdir completions
-
-for sh in bash zsh fish; do
-	go run ./cmd/container-use completion "$sh" >"completions/container-use.$sh"
-	go run ./cmd/container-use completion --command-name=cu "$sh" >"completions/cu.$sh"
-done
--- scripts/man.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-set -e
-rm -rf man
-mkdir man
-go run ./cmd/container-use man > "man/container-use.1"
